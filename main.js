1ce947acd6ec2bc9e8f50afb97a660764aff6514
Grace <graceodonnell2013@gmail.com>
2024-05-20 20:40:50 +0100

    Create table of books using template

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function Book(title, author, pageNumber, read) {
 
 const book1 = new Book("I am the Best", "Grace O Donnell", 42, true);
 myLibrary.push(book1);
-myLibrary.push(new Book("This is not my Voice on TV", "JT", 51, true));
+myLibrary.push(new Book("This is not my Voice on TV", "JT", 51, false));
 
 myLibrary.forEach((book) => console.log(book));
 myLibrary.forEach((book) => console.log(book.info()));
@@ -22,3 +22,16 @@ myLibrary.forEach((book) => console.log(book.info()));
 function addBookToLibrary() {
   // do stuff here
 }
+
+const tbody = document.querySelector("tbody");
+const template = document.querySelector("#bookrow");
+
+myLibrary.forEach((b) => {
+  const clone = template.content.cloneNode(true);
+  let td = clone.querySelectorAll("td");
+  td[0].textContent = b.title;
+  td[1].textContent = b.author;
+  td[2].textContent = b.pageNumber;
+  td[3].textContent = b.read ? "Read" : "Not read";
+  tbody.appendChild(clone);
+});
